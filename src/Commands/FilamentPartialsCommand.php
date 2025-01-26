0d349cf89e8e00f74b659ec8012fe77b4d2fe8d8
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2025-01-26 10:26:45 -0300

    feat: create stubs

--- src/Commands/FilamentPartialsCommand.php
+++ src/Commands/FilamentPartialsCommand.php
@@ -3,17 +3,74 @@
 namespace AndreFelipe\FilamentPartials\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
 
 class FilamentPartialsCommand extends Command
 {
-    public $signature = 'make:filament-partials';
+    public $signature = 'make:filament-partials {resource?}';
 
-    public $description = 'My command';
+    public $description = 'Create partials of a resource';
 
     public function handle(): int
     {
-        $this->comment('All done');
+        $resource = $this->getResourcePath();
 
-        return self::SUCCESS;
+        $this->createPartial('form', $resource);
+        $this->createPartial('table', $resource);
+
+        $this->info('Partials created successfully.');
+
+        return 0;
+    }
+
+    protected function getResourcePath(): string
+    {
+        $resource = $this->argument('resource');
+
+        if (! $resource) {
+            $resource = $this->ask('What is the resource name?');
+        }
+
+        if (! $this->isValidResourcePath($resource)) {
+            $this->error('Invalid resource path');
+            return $this->getResourcePath();
+        }
+
+        return $resource;
+    }
+
+    protected function isValidResourcePath(string $resource): bool
+    {
+        $baseResourcesPath = config('filament-partials.base_resources_path');
+        $baseClustersPath = config('filament-partials.base_clusters_path');
+
+        $resourcePath = base_path($baseResourcesPath . '/' . $resource);
+        $clusterResourcePath = base_path($baseClustersPath . '/*/Resources/' . $resource);
+
+        if (is_dir($resourcePath)) {
+            return true;
+        }
+
+        $clusters = glob(base_path($baseClustersPath . '/*'), GLOB_ONLYDIR);
+
+        foreach ($clusters as $cluster) {
+            if (is_dir($cluster . '/Resources/' . $resource)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function createPartial(string $type, string $resource): void
+    {
+        $stubPath = __DIR__ . '/../stubs/' . $type . '.stub';
+        $partialPath = base_path(config('filament-partials.base_resources_path') . '/' . $resource . '/' . ucfirst($type) . '.php');
+
+        $stub = File::get($stubPath);
+        $content = str_replace('{{ resource }}', $resource, $stub);
+
+        File::ensureDirectoryExists(dirname($partialPath));
+        File::put($partialPath, $content);
     }
 }
--- src/FilamentPartialsServiceProvider.php
+++ src/FilamentPartialsServiceProvider.php
@@ -10,16 +10,9 @@ class FilamentPartialsServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('filament-partials')
             ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_filament_partials_table')
             ->hasCommand(FilamentPartialsCommand::class);
     }
 }
