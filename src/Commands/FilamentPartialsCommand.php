7585a2fba8a8976487a10c591c87b5c7c53404f6
André Domingues <96439642+andrefelipe18@users.noreply.github.com>
2025-01-26 11:30:14 -0300

    feat: remove original method in resource

--- src/Commands/FilamentPartialsCommand.php
+++ src/Commands/FilamentPartialsCommand.php
@@ -7,13 +7,18 @@ use Illuminate\Support\Facades\File;
 
 class FilamentPartialsCommand extends Command
 {
-    public $signature = 'make:filament-partials {resource?}';
+    public $signature = 'make:filament-partials {resource?} {--only=}';
 
     public $description = 'Create partials of a resource';
 
     public function handle(): int
     {
         $resource = $this->getResourcePath();
+
+        $options = [
+            'form',
+            'table',
+        ];
 
         if (empty($resource)) {
             return 1;
@@ -23,8 +28,23 @@ class FilamentPartialsCommand extends Command
 
         $namespace = $this->getNamespace($resource['path'], $resource['name']);
 
-        $this->createPartial('form', $resource, $namespace);
-        $this->createPartial('table', $resource, $namespace);
+        $only = $this->option('only');
+        if ($only) {
+            $selectedOptions = explode(',', $only);
+            foreach ($selectedOptions as $option) {
+                $option = trim($option);
+                $option = strtolower($option);
+                if (!in_array($option, $options)) {
+                    $this->error("Invalid option: {$option}. Allowed options are: " . implode(', ', $options));
+                    return 1;
+                }
+            }
+            $options = $selectedOptions;
+        }
+
+        foreach ($options as $option) {
+            $this->createPartial($option, $resource, $namespace);
+        }
 
         $this->info('Partials created successfully.');
 
@@ -35,7 +55,7 @@ class FilamentPartialsCommand extends Command
     {
         $resource = $this->argument('resource');
 
-        if (! $resource) {
+        if (!$resource) {
             $resource = $this->ask('What is the resource name?');
         }
 
@@ -44,7 +64,6 @@ class FilamentPartialsCommand extends Command
         if (empty($resourcePath)) {
             $this->error('Invalid resource path');
             $this->error('Please check if the resource exists in the base resources path or in any cluster');
-
             return [];
         }
 
@@ -59,14 +78,14 @@ class FilamentPartialsCommand extends Command
         $baseResourcesPath = config('filament-partials.base_resources_path');
         $baseClustersPath = config('filament-partials.base_clusters_path');
 
-        $resourcePath = base_path($baseResourcesPath.'/'.$resource);
+        $resourcePath = base_path($baseResourcesPath . '/' . $resource);
         if (is_dir($resourcePath)) {
             return [$resourcePath];
         }
 
-        $clusters = glob(base_path($baseClustersPath.'/*'), GLOB_ONLYDIR);
+        $clusters = glob(base_path($baseClustersPath . '/*'), GLOB_ONLYDIR);
         foreach ($clusters as $cluster) {
-            $clusterResourcePath = $cluster.'/Resources/'.$resource;
+            $clusterResourcePath = $cluster . '/Resources/' . $resource;
             if (is_dir($clusterResourcePath)) {
                 return [$clusterResourcePath];
             }
@@ -77,16 +96,16 @@ class FilamentPartialsCommand extends Command
 
     protected function createPartialsDirectory(string $resourcePath): void
     {
-        $partialsPath = $resourcePath.'/Partials';
+        $partialsPath = $resourcePath . '/Partials';
 
-        if (! is_dir($partialsPath)) {
+        if (!is_dir($partialsPath)) {
             File::makeDirectory($partialsPath);
         }
     }
 
     protected function getNamespace(string $resourcePath, string $resourceName): string
     {
-        $resourceFile = glob($resourcePath.'.php')[0];
+        $resourceFile = glob($resourcePath . '.php')[0];
 
         $content = File::get($resourceFile);
 
@@ -110,17 +129,21 @@ class FilamentPartialsCommand extends Command
 
     protected function createPartial(string $partial, array $resource, string $namespace): void
     {
-        $stub = File::get(__DIR__.'/../stubs/'.$partial.'.stub');
+        $filename = $resource['name'] . ucfirst($partial) . '.php';
+        $partialPath = $resource['path'] . '/Partials/' . $filename;
+
+        if (File::exists($partialPath)) {
+            $this->info("Partial {$filename} already exists. Skipping creation.");
+            return;
+        }
+
+        $stub = File::get(__DIR__ . '/../stubs/' . $partial . '.stub');
 
         $modelName = str_replace('Resource', '', $resource['name']);
 
         $stub = str_replace('{{ namespace }}', $namespace, $stub);
         $stub = str_replace('{{ resource }}', $resource['name'], $stub);
         $stub = str_replace('{{ model }}', $modelName, $stub);
-
-        $filename = $resource['name'].ucfirst($partial).'.php';
-
-        $partialPath = $resource['path'].'/Partials/'.$filename;
 
         File::put($partialPath, $stub);
 
@@ -129,14 +152,22 @@ class FilamentPartialsCommand extends Command
 
     protected function addTraitToResource(string $resourcePath, string $resourceName, string $namespace, string $partial): void
     {
-        $resourceFile = $resourcePath.'.php';
-        $trait = "\\{$namespace}\\{$resourceName}".ucfirst($partial);
+        $resourceFile = $resourcePath . '.php';
+        $trait = "\\{$namespace}\\{$resourceName}" . ucfirst($partial);
 
         $content = File::get($resourceFile);
 
-        if (! str_contains($content, "use {$trait};")) {
-            $content = preg_replace('/(class\s+'.$resourceName.'\s+extends\s+Resource\s*\{)/', "$1\n    use {$trait};", $content);
-            File::put($resourceFile, $content);
+        if (!str_contains($content, "use {$trait};")) {
+            $content = preg_replace('/(class\s+' . $resourceName . '\s+extends\s+Resource\s*\{)/', "$1\n    use {$trait};", $content);
         }
+
+        $method = match ($partial) {
+            'form' => 'form',
+            'table' => 'table',
+        };
+
+        $content = preg_replace('/public\s+static\s+function\s+' . $method . '\(.*?\}\s*\n/s', '', $content);
+
+        File::put($resourceFile, $content);
     }
 }
